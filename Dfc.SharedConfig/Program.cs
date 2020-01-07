3a49a2ab8bd1ed93d39c22132cd129eb493d9a4a
Richard Lee <ric@dicci.co.uk>
2020-01-07 12:07:05 +0000

    Default setup

---
+++ Dfc.SharedConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dfc.SharedConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
